86b8ebc0ae2db20a94a03ca83817f8ee90ab52f7
Jordan <lav.jordan@live.com>
2022-06-04 20:07:16 -0700

    add hover class + mouseover listener to grid

--- java.js
+++ java.js
@@ -1,7 +1,17 @@
-for (let i = 0; i < 16; i++) {
-    for (let j =0; j < 16; j++) {
-        const block = document.createElement('div');
-        block.classList.add('block')
-        document.querySelector(".grid").appendChild(block);
+let block = [];
+let blockid = 0; 
+for (let i = 0; i < 16; i++) {  //Create grid
+    for (let j = 0; j < 16; j++) {
+        block[blockid] = document.createElement('div');
+        block[blockid].classList.add('block');
+        block[blockid].setAttribute('id', `${blockid}`);
+        document.querySelector(".grid").appendChild(block[blockid]);
+        blockid++;
     }
+}
+
+for (i = 0; i < block.length; i++) {
+    block[i].addEventListener("mouseenter", e => {
+        e.target.classList.add('hover')
+    })
 }
